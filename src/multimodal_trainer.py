3c60f3665db7fd94931db8155bf6b2f3e64c02d3
Bryan <bryan.cardenasguevara@surf.nl>
2025-04-01 14:23:49 +0200

    comment

--- src/multimodal_trainer.py
+++ src/multimodal_trainer.py
@@ -2,7 +2,14 @@ from typing import Any, Dict
 from transformers import Trainer
 
 class MultiModalTrainer(Trainer):
-    def compute_loss(self, model: Any, inputs: Dict[str, Any], return_outputs: bool = False):
+    def compute_loss(self, 
+                     model: Any, 
+                     inputs: Dict[str, Any], 
+                     return_outputs: bool = False):
+        """
+        Override the compute_loss from hf trainer and include the wsi_embeddings to our modified
+        model class.
+        """
         wsi_embeddings = inputs.pop("wsi_embeddings", None)
         outputs = model(**inputs, wsi_embeddings=wsi_embeddings)
         loss = outputs.loss
