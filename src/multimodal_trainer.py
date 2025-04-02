fef2772419d6068a8eb58fb410cb495fafd78883
Bryan <bryan.cardenasguevara@surf.nl>
2025-04-02 15:34:52 +0200

    added logger, a model size printer and added collator

--- src/multimodal_trainer.py
+++ src/multimodal_trainer.py
@@ -5,10 +5,13 @@ class MultiModalTrainer(Trainer):
     def compute_loss(self, 
                      model: Any, 
                      inputs: Dict[str, Any], 
-                     return_outputs: bool = False):
+                     return_outputs: bool = False,
+                     num_items_in_batch: str = None):
         """
         Override the compute_loss from hf trainer and include the wsi_embeddings to our modified
         model class.
+
+        We can also give this function to the trainer with argument: "compute_loss_func".
         """
         wsi_embeddings = inputs.pop("wsi_embeddings", None)
         outputs = model(**inputs, wsi_embeddings=wsi_embeddings)
